fab40544320b991826656de86f31cedda63eadd0
Thermick <thermick@gmail.com>
2025-07-16 12:03:33 -0600

    Deploy DataFactory Module

--- CentroContactoDatos/main.tf
+++ CentroContactoDatos/main.tf
@@ -39,51 +39,65 @@ data "azurerm_network_security_group" "private_nsg" {
 
 # --- LLAMADA AL MÓDULO DE DATABRICKS ---
 module "databricks_contact_center" {
-  # IMPORTANTE: Reemplaza la URL con la de tu repositorio de GitHub.
-  # Usa una nueva versión/tag que refleje los últimos cambios (ej. v1.4.0).
-  source = "git::https://github.com/tu-organizacion/AzNativeServices.git//Mod_AzDatabricks_Native?ref=v1.4.0"
+  source = "../AzNativeServices/Mod_AzDatabricks_Native"
 
   # --- Parámetros Generales ---
-  resource_group_name            = "z-nsm-contactcenter-pp01-ue2-01"
-  workspace_name                 = "dbrk-contactcenter-pp01-ue2-01"
-  managed_resource_group_name    = "z-nsm-contactcenter-pp01-ue2-01-dbmng"
-  
-  # --- Parámetros de Red ---
+  resource_group_name         = "z-nsm-contactcenter-pp01-ue2-01"
+  workspace_name              = "dbrk-contactcenter-pp01-ue2-01"
+  managed_resource_group_name = "z-nsm-contactcenter-pp01-ue2-01-dbmng"
+
+  # --- Parámetros de Red (VNet Injection) ---
   databricks_vnet_name           = "znsmccentercintpp01eu2net01"
   databricks_vnet_rg_name        = "z-nsm-ccentercint-pp01-ue2-01"
   databricks_public_subnet_name  = "databrickspub64-pic-rt"
   databricks_private_subnet_name = "databrickspriv64-pic-rt"
-  # --- NUEVOS PARÁMETROS PARA LOS NSG ---
   public_subnet_nsg_id           = data.azurerm_network_security_group.public_nsg.id
   private_subnet_nsg_id          = data.azurerm_network_security_group.private_nsg.id
-  # --- NUEVOS PARÁMETROS PARA EL PRIVATE ENDPOINT ---
-  private_endpoint_name          = "private-endpoint-databricks-pic-rt" # <-- CAMBIA ESTO
-  private_endpoint_vnet_name     = "znsmccintpp01ue2net01"
-  private_endpoint_vnet_rg_name  = "z-nsm-ccint-pp01-ue2-01"
-  private_endpoint_subnet_name   = "main-pic-rt"
-  
+
+  # --- Parámetros de Red (Private Endpoint) ---
+  private_endpoint_vnet_name    = "znsmccintpp01ue2net01"
+  private_endpoint_vnet_rg_name = "z-nsm-ccint-pp01-ue2-01"
+  private_endpoint_subnet_name  = "main-pic-rt"
 
   # --- CONFIGURACIÓN DE DNS ---
-  # Elige UNO de los siguientes 3 escenarios y comenta los otros dos.
-
-  # --- Escenario 1: CREAR una nueva Zona DNS (Configuración Actual) ---
+  # Escenario 1: CREAR una nueva Zona DNS.
   enable_private_dns_integration = true
   create_private_dns_zone        = true
-
-  # --- Escenario 2: USAR una Zona DNS existente ---
-  # Comenta el Escenario 1 y descomenta las siguientes 3 líneas.
-  # enable_private_dns_integration = true
-  # create_private_dns_zone        = false # Opcional, es el valor por defecto
+  # Para usar una zona existente:
+  # create_private_dns_zone        = false
   # private_dns_zone_id_databricks = data.azurerm_private_dns_zone.dns_existente.id
-
-  # --- Escenario 3: SIN integración con DNS ---
-  # Comenta los Escenarios 1 y 2 y descomenta la siguiente línea.
-  # enable_private_dns_integration = false
-
 
   # --- Habilitar creación de la cuenta de almacenamiento ---
   create_storage_account = true
   storage_account_name   = "stcontactcenterpp01ue2"
+
+  tags = {
+    Proyecto = "CentroContactoDatos"
+    Ambiente = "Piloto-Produccion"
+    Owner    = "EquipoDeDatos"
+  }
+}
+
+# --- LLAMADA AL MÓDULO DE DATA FACTORY ---
+module "data_factory_contact_center" {
+  source = "../AzNativeServices/Mod_AzDataFactory_Native"
+
+  # --- Parámetros Generales ---
+  resource_group_name = "z-nsm-contactcenter-pp01-ue2-01"
+  data_factory_name   = "adf-contactcenter-pp01-ue2-01"
+
+  # --- Parámetros de Red (Private Endpoint) ---
+  private_endpoint_vnet_name    = "znsmccintpp01ue2net01"
+  private_endpoint_vnet_rg_name = "z-nsm-ccint-pp01-ue2-01"
+  private_endpoint_subnet_name  = "main-pic-rt"
+
+  # --- CONFIGURACIÓN DE DNS ---
+  # Escenario 1: CREAR una nueva Zona DNS.
+  enable_private_dns_integration = true
+  create_private_dns_zone        = true
+  # Para usar una zona existente:
+  # create_private_dns_zone        = false
+  # private_dns_zone_id_datafactory = data.azurerm_private_dns_zone.dns_existente_adf.id
 
   tags = {
     Proyecto = "CentroContactoDatos"
